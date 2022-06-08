27b9387fded3f9ade2f516edb60d277e1b085a22
LuuckA21 <luca.dalu@outlook.it>
2022-06-08 19:52:45 +0200

    Add 1.19 support

--- src/main/java/me/luucka/teleportbow/utils/ServerVersion.java
+++ src/main/java/me/luucka/teleportbow/utils/ServerVersion.java
@@ -8,14 +8,11 @@ public final class ServerVersion {
 
     public static final int MINOR;
 
-    public static final int PATCH;
-
     private ServerVersion() {}
 
     static {
         final String[] version = Bukkit.getBukkitVersion().substring(0, Bukkit.getBukkitVersion().indexOf("-")).split("\\.");
         MAJOR = Integer.parseInt(version[0]);
         MINOR = Integer.parseInt(version[1]);
-        PATCH = Integer.parseInt(version[2]);
     }
 }
